77cf3eb66b2e40a3b284b885e3f1b949c032d559
alanmcode <68718613+alanmcode@users.noreply.github.com>
2022-12-13 17:28:37 +0000

    app.vue and style.css

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -1,7 +1,9 @@
 <template>
 <div>
   <nav class="navbar navbar-expand-lg navbar-light bg-light">
-  <a class="navbar-brand" href="#">Navbar w/ text</a>
+    <div>
+  <a class="navbar-brand" href="#">Auction</a>
+    </div>
   <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarText" aria-controls="navbarText" aria-expanded="false" aria-label="Toggle navigation">
     <span class="navbar-toggler-icon"></span>
   </button>
@@ -16,17 +18,68 @@
       <li class="nav-item">
         <a class="nav-link" href="#">Pricing</a>
       </li>
+      <li class="nav-item">
+        <button onclick="document.getElementById('login').style.display='block'" style="width:auto;">Login / Sign In</button>
+      </li>
     </ul>
     <form class="form-inline">
     <input class="form-control mr-sm-2" type="search" placeholder="Search" aria-label="Search">
-    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+    <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>  
   </form>
   </div>
 </nav>
   <div>
   </div>
 
+
+
 </div>
+
+<!-- login -->
+  
+  <form class="" method="post">
+    <div class="container">
+      <label for="email"><b>Email</b></label>
+      <input type="email" placeholder="Enter Email" name="email" required>
+
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required>
+        
+      <button type="submit">Login</button>
+      <label>
+        <input type="checkbox" checked="checked" name="remember"> Remember me
+      </label>
+    </div>
+
+    <div class="container" >
+      <button type="button" onclick="document.getElementById('login').style.display='none'" class="cancelbtn">Cancel</button>
+    </div>
+  </form>
+
+
+<!-- signup -->
+
+<form class="" method="post">
+    <div class="container">
+      <label for="email"><b>Email</b></label>
+      <input type="email" placeholder="Enter Email" name="email" required>
+
+      <label for="psw"><b>Password</b></label>
+      <input type="password" placeholder="Enter Password" name="psw" required>
+
+      <label for="dob"><b>Date of Birth</b></label>
+      <input type="date" placeholder="Enter Date of Birth" name="dob" required>
+        
+      <button type="submit">Sign Up</button>
+      <label>
+        <input type="checkbox" checked="checked" name="remember"> Remember me
+      </label>
+    </div>
+
+    <div class="container" >
+      <button type="button" onclick="document.getElementById('login').style.display='none'" class="cancelbtn">Cancel</button>
+    </div>
+  </form>
 
 </template>
 
@@ -35,7 +88,7 @@
 export default {
   data() {
     return {
-      name: "Sasi",
+      name: "abc",
       counter: 0,
     }
   }
