8f18ccadf5d375e6fb3a86d172a9b235459de6cc
SaadAb4 <78376095+SaadAb4@users.noreply.github.com>
2022-12-14 15:26:18 +0000

    Added all the websites
    done frontend for now

--- cw3/frontend/src/router/index.ts
+++ cw3/frontend/src/router/index.ts
@@ -1,18 +1,25 @@
 import { createRouter, createWebHistory} from 'vue-router'
-//import Home from '../views/Home.vue'
-//import About from '../views/About.vue'
-const Home = { template: '<div>Home</div>' }
-const About = { template: '<div>About</div>' }
+import Home from '../views/Home.vue'
+import About from '../views/About.vue'
+import LogIn from '../views/login.vue'
+import Profile from '../views/profilepage.vue'
 
 const routes = [
   {
     path: '/', 
+    name:"Home",
     component: Home
   },
 
   {
-    path: '/about',
-    component: About
+    path: '/profile',
+    name:"Profile",
+    component: Profile
+  },
+  {
+    path: '/login', 
+    name:"login",
+    component: LogIn
   }
 ]
 
